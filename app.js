421773777f220b541b9ff68bc5a4fc08bbaa99d5
Maciej Kurek <maciejk77@gmail.com>
2016-07-23 16:42:50 +0100

    One player game enginge completed, points value logic for Ace fixed for the moment

--- app.js
+++ app.js
@@ -7,63 +7,70 @@
 // To be rewritten in construcor function, prototype methods or some JS pattern
 
 var cards = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
+var colours = ['S', 'C', 'H', 'D']; // Spades, Clubs, Hearts, Diamonds
 var deck = [];
 var result = 0;
-var playerCards = getDeckOf(cards);
 
-// quick fix to add 3 more suits, a deck in total
-function getDeckOf(cards) {
-  cards.forEach(function (card) {
-    for(var i = 0; i < 4; i++) {
-      deck.push(card); 
-    }
-  });
-  shuffle(deck);
-  return deck;  
+// Initialize a deck of cards
+getDeck(cards, colours);
+
+// Return deck of cards based on cards and colours array
+function getDeck(cards, colours) {
+  cards.forEach(function(card) {
+    colours.forEach(function(colour) {
+      deck.push(colour.toString() + card.toString());
+    });
+  });    
 }
 
-function shuffle(cards) {
+// Shuffling cards in the array of cards provided
+function shuffleCards(cards) {
 
   var j, x, i;
   for (i = cards.length; i; i--) {
-      j = Math.floor(Math.random() * i);
-      x = cards[i - 1];
-      cards[i - 1] = cards[j];
-      cards[j] = x;
+    j = Math.floor(Math.random() * i);
+    x = cards[i - 1];
+    cards[i - 1] = cards[j];
+    cards[j] = x;
   }
 
 }
 
-function deal(deck) {
+// Get the last card from deck and remove that card from deck 
+function dealACard(deck) {
 
   var card = deck.pop();
-  var dealtCard = check(card);
+  var dealtCard = checkValueOf(card);
 
   result += dealtCard;
 
   if(result < 21) {
-    console.log(result + ' is the current result');
+    console.log(result + ' is the current result'); // to be removed
   } else if(result === 21) {
     console.log(result + ' is the Blackjack!');
   } else {
-    console.log(result + ' ...This is over 21 !!');
+    console.log(result + ' ...this is over 21, GAME OVER!!'); // to be removed
   }
 
 }
 
-function check(card) {
-  
-  switch(card) {
+// Return the points value of a given card
+function checkValueOf(card) {
+
+  var el = card.slice(1);
+  isNaN(el) ? el = el.toString() : el = parseInt(el);
+
+  switch(el) {
     case 'J':
     case 'K':
     case 'Q':
         return 10;
         break;
     case 'A':
-        return result + 11 < 21 ? 11 : 1;
+        return result < 21 ? 11 : 1; // fixed logic for the moment
         break;
     default:
-        return card;
+        return el;
   }
 
 };
