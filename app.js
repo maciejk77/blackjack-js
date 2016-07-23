93c9bcb67b27da237c77da8f48ced415036b6817
Maciej Kurek <maciejk77@gmail.com>
2016-07-23 23:57:03 +0100

    Engine and event listeners, a bug on hit(deck)

--- app.js
+++ app.js
@@ -6,28 +6,99 @@
 // Not DRY - repetion in if statement 'result' and switch refactored a bit
 // To be rewritten in construcor function, prototype methods or some JS pattern
 
+var Game = function(name) {
+  this.result = 0;
+  this.name = name;
+} 
+
+// // Caching buttons and setting their status to active
+var hitButton = document.querySelector(".js-hit");
+var stickButton = document.querySelector(".js-stick");
+// var newButton = document.querySelector(".js-newgame");
+
+// Arrays of card types
 var cards = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
 var colours = ['S', 'C', 'H', 'D']; // Spades, Clubs, Hearts, Diamonds
 var deck = [];
-var result = 0;
-
-// Initialize a deck of cards
 getDeck(cards, colours);
-// Shuffle cards
-shuffleCards(deck);
+console.log(deck);
 
-// First to random cards dealt by the Dealer
-dealACard(deck);
-dealACard(deck);
 
+// Dealer and Player initiated
+var dealer = new Game('Dealer');
+var player = new Game('Player');
+
+Game.prototype.hit = function(deck) {
+  var card = deck.pop();
+  var dealtCard = checkValueOf(card);
+
+  // Gives 1 point instead of 11 if dealt card was 11 and result is already bigger than 10
+  if(dealtCard === 11 && this.result > 10) { dealtCard = 1 };
+  this.result = this.result + dealtCard;
+
+  if(this.result < 21) {
+    console.log(this.name + ' got ' + this.result + ' points in total'); // to be removed
+  } else if(this.result === 21) {
+    console.log(this.name + ' got ' + this.result + ' WINNER!'); // to be removed
+    stickButton.disabled = true;
+    hitButton.disabled = true;
+  } else {
+    console.log(this.name + ' got ' + this.result + ' points, this is over 21, LOSER!'); // to be removed
+    stickButton.disabled = true;
+    hitButton.disabled = true;
+  }
+
+}
+
+// player.hit(deck); 
+// dealer.hit(deck);
+// player.hit(deck); 
+
+// // // //newGameAction();
+
+function hitAction() {
+  player.hit();
+}
+
+function stickAction() {
+  dealer.hit();
+}
+
+// // Event listeners, binding buttons
+hitButton.addEventListener('click', hitAction, false); 
+stickButton.addEventListener('click', stickAction, false); 
+// // // newButton.addEventListener('click', newAction, false); 
+
+
+// function newAction() {
+//   // player.result = 0;
+//   // dealer.result = 0;
+//   // stickButton.disabled = false;
+//   // hitButton.disabled = false;
+
+//   // // Initialize a deck of cards
+//   // getDeck(cards, colours);
+//   // // Shuffle cards
+//   // shuffleCards(deck);
+
+//   // //Game initialization
+//   // player.hit(deck);
+//   // dealer.hit(deck);
+//   // player.hit(deck);
+//   console.log('TESTING 123');
+// }
 
 // Return deck of cards based on cards and colours array
 function getDeck(cards, colours) {
+  
   cards.forEach(function(card) {
+    
     colours.forEach(function(colour) {
       deck.push(colour.toString() + card.toString());
     });
+  
   });    
+
 }
 
 // Shuffling cards in the array of cards provided
@@ -43,24 +114,6 @@ function shuffleCards(cards) {
 
 }
 
-// Get the last card from deck and remove that card from deck 
-function dealACard(deck) {
-
-  var card = deck.pop();
-  var dealtCard = checkValueOf(card);
-
-  result += dealtCard;
-
-  if(result < 21) {
-    console.log(result + ' is the current result'); // to be removed
-  } else if(result === 21) {
-    console.log(result + ' is the Blackjack!');
-  } else {
-    console.log(result + ' ...this is over 21, GAME OVER!!'); // to be removed
-  }
-
-}
-
 // Return the points value of a given card
 function checkValueOf(card) {
 
@@ -71,14 +124,23 @@ function checkValueOf(card) {
     case 'J':
     case 'K':
     case 'Q':
-        return 10;
-        break;
+      return 10;
+      break;
     case 'A':
-        return result < 21 ? 11 : 1; // fixed logic for the moment
-        break;
+      return 11;
+      break;
     default:
-        return el;
+      return el;
   }
 
 };
+
+// // Initialization of the game
+// getDeck(cards, colours);
+// shuffleCards(deck);
+// // hitButton.disabled = false;
+// // stickButton.disabled = false;
+// console.log(deck);
+// console.log(deck.length);
+// player.hit(deck);
 
