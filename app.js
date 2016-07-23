893fe471321d197ed6495e8f4a169341e3087639
Maciej Kurek <maciejk77@gmail.com>
2016-07-23 14:32:08 +0100

    Deck generation added, repetition in if statement fixed

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 // Basic game engine
 // Assumptions: 
-// 1 player, 1 suit of cards
+// 1 player, 1 deck of cards
 // Just a result counter, no option to stop based on result
 // Logic around Ace is to add 11 if sum below 21 otherwise 1
-// Not DRY - repetion in if statement 'result' and switch not the cleanest
+// Not DRY - repetion in if statement 'result' and switch refactored a bit
 // To be rewritten in construcor function, prototype methods or some JS pattern
 
 var cards = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
+var deck = [];
 var result = 0;
+var playerCards = getDeckOf(cards);
+
+// quick fix to add 3 more suits, a deck in total
+function getDeckOf(cards) {
+  cards.forEach(function (card) {
+    for(var i = 0; i < 4; i++) {
+      deck.push(card); 
+    }
+  });
+  shuffle(deck);
+  return deck;  
+}
 
 function shuffle(cards) {
 
@@ -21,19 +34,19 @@ function shuffle(cards) {
 
 }
 
-function deal(cards) {
+function deal(deck) {
 
-  var card = cards.pop();
+  var card = deck.pop();
   var dealtCard = check(card);
 
-  if (result + dealtCard < 21) {
-    result += dealtCard;
+  result += dealtCard;
+
+  if(result < 21) {
     console.log(result + ' is the current result');
-  } else if(result + dealtCard === 21) {
-    result += dealtCard;
+  } else if(result === 21) {
     console.log(result + ' is the Blackjack!');
   } else {
-    console.log('This is over 21 !!');
+    console.log(result + ' ...This is over 21 !!');
   }
 
 }
