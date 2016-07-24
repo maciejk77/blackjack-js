77084a89f39e0d7e2d456c90831d82ee1cdcfc4c
Maciej Kurek <maciejk77@gmail.com>
2016-07-24 13:17:45 +0100

    Bootstrap classed and automatic dealer to be fixed still

--- app.js
+++ app.js
@@ -3,11 +3,6 @@
 // 1 player, 1 deck of cards
 // Logic around Ace is to add 11 if sum below 21 otherwise 1
 
-var Game = function(name) {
-  this.result = 0;
-  this.name = name;
-}
-
 var cards = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
 var colours = ['S', 'C', 'H', 'D']; // Spades, Clubs, Hearts, Diamonds
 var deck = [];
@@ -15,7 +10,66 @@ var result = 0;
 var hitButton = document.querySelector(".js-hit");
 var stickButton = document.querySelector(".js-stick");
 var resetButton = document.querySelector(".js-reset");
+var textEl = document.querySelector(".js-msg");
 
+var Game = function(name) {
+  this.result = 0;
+  this.name = name;
+}
+
+// Initialization of two players
+var player = new Game('Player');
+var dealer = new Game('Dealer');
+
+// Get the last card from deck and remove that card from deck 
+Game.prototype.hit = function(deck) {
+
+  var card = deck.pop();
+  var dealtCard = checkValueOf(card);
+
+  if(dealtCard === 11 && this.result > 10 ) { dealtCard = 1; }
+  this.result = this.result + dealtCard;
+
+  if(this.result < 21) {
+    textEl.innerHTML = (this.name + ' got ' + this.result + ' points in total'); 
+    console.log(this.name + ' got ' + this.result + ' points in total'); // to be removed
+  } else if(this.result === 21) {
+      textEl.innerHTML = (this.name + ' got ' + this.result + ' a Blackjack!!');
+      console.log(this.name + ' got ' + this.result + ' a Blackjack!!'); // to be removed
+      disableButtons();
+  } else {
+      textEl.innerHTML = (this.name + ' got ' + this.result + ' points, that is GAME OVER!!');
+      console.log(this.name + ' got ' + this.result + ' points, that is GAME OVER!!'); // to be removed
+      disableButtons();
+  }
+
+}
+
+// Return the points value of a given card
+function checkValueOf(card) {
+
+  var el = card.slice(1);
+  isNaN(el) ? el = el.toString() : el = parseInt(el);
+
+  switch(el) {
+    case 'J':
+    case 'K':
+    case 'Q':
+        return 10;
+        break;
+    case 'A':
+        return 11;
+        break;
+    default:
+        return el;
+  }
+
+};
+
+function disableButtons() {
+  hitButton.disabled = true;
+  stickButton.disabled = true;
+}
 
 // Return deck of cards based on cards and colours array
 function getDeck(cards, colours) {
@@ -39,71 +93,21 @@ function shuffleCards(cards) {
 
 }
 
-// Get the last card from deck and remove that card from deck 
-Game.prototype.hit = function(deck) {
-
-  var card = deck.pop();
-  var dealtCard = checkValueOf(card);
-
-  if(dealtCard === 11 && this.result > 10 ) { dealtCard = 1; }
-  this.result = this.result + dealtCard;
-
-  if(this.result < 21) {
-    console.log(this.name + ' got ' + this.result + ' points in total'); // to be removed
-  } else if(this.result === 21) {
-    console.log(this.name + ' got ' + this.result + ' a Blackjack!!');
-    hitButton.disabled = true;
-    stickButton.disabled = true;
-  } else {
-    console.log(this.name + ' got ' + this.result + ' points, that is GAME OVER!!'); // to be removed
-    hitButton.disabled = true;
-    stickButton.disabled = true;
-  }
-
-}
-
-var player = new Game('Player');
-var dealer = new Game('Dealer');
-
-function startGame() {
-  // Initialize a deck of cards
-  getDeck(cards, colours);
-  // Shuffle cards
-  shuffleCards(deck);
-  
-  player.hit(deck);
-  dealer.hit(deck);
-  player.hit(deck); 
-}
-
-// Return the points value of a given card
-function checkValueOf(card) {
-
-  var el = card.slice(1);
-  isNaN(el) ? el = el.toString() : el = parseInt(el);
-
-  switch(el) {
-    case 'J':
-    case 'K':
-    case 'Q':
-        return 10;
-        break;
-    case 'A':
-        return 11; // fixed logic for the moment
-        break;
-    default:
-        return el;
-  }
-
-};
-
 
 function hitAction() {
-  player.hit(deck);
+  // An attempt to create automated dealer after reaching 17 points
+  // player.hit(deck);
+  // if(dealer.result > 17) {
+    player.hit(deck);
+  // }
 };
 
 function stickAction() {
-  dealer.hit(deck);
+  // An attempt to create automated dealer after reaching 17 points
+  // while(dealer.result <= 17) {
+    dealer.hit(deck);
+  // }
+
 };
 
 function resetAction() {
@@ -111,12 +115,23 @@ function resetAction() {
   player.result = 0;
   hitButton.disabled = false;
   stickButton.disabled = false;
+  deck = [];
   startGame();
 };
 
+
+function startGame() {
+  getDeck(cards, colours);
+  shuffleCards(deck);
+  
+  //Deal 2 cards to the Player and one to the Dealer
+  player.hit(deck);
+  dealer.hit(deck);
+  player.hit(deck); 
+}
+
+
+// Event listeners for Hit, Stick and Reset buttons
 hitButton.addEventListener('click', hitAction, false);
 stickButton.addEventListener('click', stickAction, false);
 resetButton.addEventListener('click', resetAction, false);
-
-// Starting a game
-startGame();
